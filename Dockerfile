6b9985d34340c432d1d6eb45838dedfa0c657fdf
Darren Shepherd <darren@rancher.com>
2015-07-29 00:10:41 -0700

    Separate setting up fs and launching

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM scratch
 ADD build/base-files.tar.gz /
-COPY build/ca-certificates.crt /etc/ssl/certs/
+COPY build/ca-certificates.crt /usr/etc/ssl/certs/
 COPY build/dockerlaunch /usr/bin/
 COPY build/docker /usr/bin/docker
 VOLUME /var/lib/docker
--- main/main.go
+++ main/main.go
@@ -2,26 +2,10 @@ package main
 
 import (
 	"os"
-	"strconv"
-	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	dockerlaunch "github.com/rancher/docker-from-scratch"
 )
-
-func getValue(index int, args []string) string {
-	val := args[index]
-	parts := strings.SplitN(val, "=", 2)
-	if len(parts) == 1 {
-		if len(args) > index+1 {
-			return args[index+1]
-		} else {
-			return ""
-		}
-	} else {
-		return parts[2]
-	}
-}
 
 func main() {
 	if os.Getenv("DOCKER_LAUNCH_DEBUG") == "true" {
@@ -38,45 +22,11 @@ func main() {
 	}
 
 	var config dockerlaunch.Config
-
-	for i, arg := range args {
-		if strings.HasPrefix(arg, "--bip") {
-			config.BridgeAddress = getValue(i, args)
-		} else if strings.HasPrefix(arg, "-b") || strings.HasPrefix(arg, "--bridge") {
-			config.BridgeName = getValue(i, args)
-		} else if strings.HasPrefix(arg, "--mtu") {
-			mtu, err := strconv.Atoi(getValue(i, args))
-			if err != nil {
-				config.BridgeMtu = mtu
-			}
-		}
-	}
-
-	if config.BridgeName != "" && config.BridgeAddress != "" {
-		newArgs := []string{}
-		skip := false
-		for _, arg := range args {
-			if skip {
-				skip = false
-				continue
-			}
-
-			if arg == "--bip" {
-				skip = true
-				continue
-			} else if strings.HasPrefix(arg, "--bip=") {
-				continue
-			}
-
-			newArgs = append(newArgs, arg)
-		}
-
-		args = newArgs
-	}
+	args = dockerlaunch.ParseConfig(&config, args...)
 
 	log.Debugf("Launch config %#v", config)
 
-	err := dockerlaunch.LaunchDocker(&config, os.Args[1], args...)
+	_, err := dockerlaunch.LaunchDocker(&config, os.Args[1], args...)
 	if err != nil {
 		log.Fatal(err)
 	}
